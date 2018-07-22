ad678527e76ab5f3ca93525b0da890cc48f50ee9
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-22 15:23:37 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,14 +3,14 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
 
   def change
     create_table :haunted_houses do |t|
-      t.string :name 
+      t.string :name
       t.string :location
       t.string :theme
-      t.float :price 
-      t.boolean :family_friendly 
-      t.string :opening_date 
+      t.float :price
+      t.boolean :family_friendly
+      t.string :opening_date
       t.string :closing_date
-    end 
+    end
   end
 
 end
