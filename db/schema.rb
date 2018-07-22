aa0d8d6a5c29ecb2e1bcb4e0694b3868c2519769
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-22 15:30:25 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "price"
     t.integer "size"
     t.string "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "haunted_houses", force: :cascade do |t|
@@ -39,7 +37,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.boolean "family_friendly"
     t.string "opening_date"
     t.string "closing_date"
-    t.string "description"
   end
 
 end
